8fd7c94e52891da86e2fe63a74f7bd129859701b
Zeus <zeus.albu@gmail.com>
2022-11-17 17:49:56 +0200

    simplified connection test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,24 @@
 package org.example;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.logging.Level;
 
 public class Main {
+
+    public static EntityManager createEm(String unitName) {
+        return Persistence.createEntityManagerFactory(unitName).createEntityManager();
+    }
+
     public static void main(String[] args) {
 
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.SEVERE); // Sets logging level
 
         // Testing mysql connection
-        EntityManagerFactory mysqlEmf = Persistence.createEntityManagerFactory("mysql");
-        EntityManager mysqlEm = mysqlEmf.createEntityManager();
-        System.out.println("Mysql connection: " + mysqlEm.isOpen());
+        System.out.println("Postgres connection: " + createEm("mysql").isOpen());
 
         // Testing postgres connection
-        EntityManagerFactory postgresEmf = Persistence.createEntityManagerFactory("postgres");
-        EntityManager postgresEm = postgresEmf.createEntityManager();
-        System.out.println("Postgres connection: " + postgresEm.isOpen());
+        System.out.println("Postgres connection: " + createEm("postgres").isOpen());
 
     }
 
